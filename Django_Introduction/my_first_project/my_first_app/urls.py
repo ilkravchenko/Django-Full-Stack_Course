e53a774565828891912c98b304318553360fd30d
Illia Kravchenko <il.kravchenko115@gmail.com>
2023-05-20 18:42:54 +0300

    Learn about Dynamic Views in Django

--- Django_Introduction/my_first_project/my_first_app/urls.py
+++ Django_Introduction/my_first_project/my_first_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), #my_first_apps --> PROJECT urls.py
+    path('<str:page>/', views.news_page, name='news_page'),
+    path('<int:num1>/<int:num2>', views.add_nums, name='add_nums')
 ]
--- Django_Introduction/my_first_project/my_first_app/views.py
+++ Django_Introduction/my_first_project/my_first_app/views.py
@@ -2,5 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+articles = {
+    'sports' : 'Sports Page',
+    'finance' : 'Finance Page',
+    'politics' : 'Politics Page',
+}
+
 def index(request):
     return HttpResponse("Hello world!")
+
+def news_page(request, page):
+    return HttpResponse(articles[page])
+
+def add_nums(request, num1, num2):
+    add_result = num1 + num2
+    result = f"{num1} + {num2} = {add_result}"
+    return HttpResponse(f'{result}')
