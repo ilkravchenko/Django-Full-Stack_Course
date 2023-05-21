4629446e4f204d146eef5ec2bca38e6a7e8a09d7
Illia Kravchenko <il.kravchenko115@gmail.com>
2023-05-21 12:32:16 +0300

    Add ResponseRedirect to project

--- Django_Introduction/my_first_project/my_first_app/urls.py
+++ Django_Introduction/my_first_project/my_first_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), #my_first_apps --> PROJECT urls.py
+    path('<int:num_page>', views.num_page_view, name='num_page_view'),
     path('<str:page>/', views.news_page, name='news_page'),
     path('<int:num1>/<int:num2>', views.add_nums, name='add_nums')
 ]
--- Django_Introduction/my_first_project/my_first_app/views.py
+++ Django_Introduction/my_first_project/my_first_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, Http404
-
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
 # Create your views here.
 
 articles = {
@@ -25,3 +24,14 @@ def add_nums(request, num1, num2):
     add_result = num1 + num2
     result = f"{num1} + {num2} = {add_result}"
     return HttpResponse(f'{result}')
+
+# redirect from domain.com/my_first_app/0 ---> domain.com/my_first_app/sports
+def num_page_view(request, num_page):
+    topics_list = list(articles.keys())
+    
+    try:
+        topic = topics_list[num_page]    
+        return HttpResponseRedirect(topic)
+    except:
+        return HttpResponseNotFound('No page for that topic')
+    
