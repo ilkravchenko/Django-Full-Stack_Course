3c49f2cf79317ede977617c427a91d1308b23402
Illia Kravchenko <il.kravchenko115@gmail.com>
2023-05-22 16:29:54 +0300

    Create first Template in Django Project

--- Django_Introduction/my_first_project/my_first_app/urls.py
+++ Django_Introduction/my_first_project/my_first_app/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), #my_first_apps --> PROJECT urls.py
-    path('<int:num_page>', views.num_page_view, name='num_page_view'),
-    path('<str:page>/', views.news_page, name='news_page'),
-    path('<int:num1>/<int:num2>', views.add_nums, name='add_nums')
+    path('', views.simple_view) # domain.com/my_first_app
+    
+    
+    # path('', views.index, name='index'), #my_first_apps --> PROJECT urls.py
+    # path('<int:num_page>', views.num_page_view, name='num_page_view'),
+    # path('<str:page>/', views.news_page, name='news_page'),
+    # path('<int:num1>/<int:num2>', views.add_nums, name='add_nums')
 ]
--- Django_Introduction/my_first_project/my_first_app/views.py
+++ Django_Introduction/my_first_project/my_first_app/views.py
@@ -3,41 +3,90 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpRespons
 from django.urls import reverse
 # Create your views here.
 
-articles = {
-    'sports' : 'Sports Page',
-    'finance' : 'Finance Page',
-    'politics' : 'Politics Page',
-}
 
-def index(request):
-    return HttpResponse("Hello world!")
 
-def news_page(request, page):
-    try:
-        result = articles[page]
-        return HttpResponse(result)
-    except:
-        result = 'No page for that topic'
-        # return HttpResponseNotFound(result)
-        raise Http404("404 GENERIC ERROR")
+def simple_view(request):
+    return render(request, 'my_first_app/example.html') # .html
 
-def add_nums(request, num1, num2):
-    add_result = num1 + num2
-    result = f"{num1} + {num2} = {add_result}"
-    return HttpResponse(f'{result}')
 
-# redirect from domain.com/my_first_app/0 ---> domain.com/my_first_app/sports
-def num_page_view(request, num_page):
-    topics_list = list(articles.keys())
-    topic = topics_list[num_page]  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# articles = {
+#     'sports' : 'Sports Page',
+#     'finance' : 'Finance Page',
+#     'politics' : 'Politics Page',
+# }
+
+# def index(request):
+#     return HttpResponse("Hello world!")
+
+# def news_page(request, page):
+#     try:
+#         result = articles[page]
+#         return HttpResponse(result)
+#     except:
+#         result = 'No page for that topic'
+#         # return HttpResponseNotFound(result)
+#         raise Http404("404 GENERIC ERROR")
+
+# def add_nums(request, num1, num2):
+#     add_result = num1 + num2
+#     result = f"{num1} + {num2} = {add_result}"
+#     return HttpResponse(f'{result}')
+
+# # redirect from domain.com/my_first_app/0 ---> domain.com/my_first_app/sports
+# def num_page_view(request, num_page):
+#     topics_list = list(articles.keys())
+#     topic = topics_list[num_page]  
     
-    webpage = reverse('news_page', args=[topic])
-    return HttpResponseRedirect(webpage)
-    # return HttpResponseRedirect(reverse('news_page', args=[topic]))
+#     webpage = reverse('news_page', args=[topic])
+#     return HttpResponseRedirect(webpage)
+#     # return HttpResponseRedirect(reverse('news_page', args=[topic]))
     
-    # try:
-    #     topic = topics_list[num_page]    
-    #     return HttpResponseRedirect(topic)
-    # except:
-    #     return HttpResponseNotFound('No page for that topic')
+#     # try:
+#     #     topic = topics_list[num_page]    
+#     #     return HttpResponseRedirect(topic)
+#     # except:
+#     #     return HttpResponseNotFound('No page for that topic')
     
