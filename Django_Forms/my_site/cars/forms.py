b25742dff1c1c2bced747e71e3f965242f30a672
Illia Kravchenko <il.kravchenko115@gmail.com>
2023-06-22 23:42:45 +0300

    Learn about ModelForms Customization and Errors handling

--- Django_Forms/my_site/cars/forms.py
+++ Django_Forms/my_site/cars/forms.py
@@ -14,4 +14,17 @@ from django.forms import ModelForm
 class ReviewForm(ModelForm):
     class Meta:
         model = Review
-        fields = ['first_name', 'last_name', 'stars']
+        fields = '__all__'
+        # fields = ['first_name', 'last_name', 'stars']
+        
+        labels = {
+            'first_name':'YOUR FIRST NAME',
+            'last_name':'Last Name',
+            'stars':'Rating:'
+        }
+        error_message = {
+            'stars':{
+                'min_value':'YO! Min value is 1',
+                'max_value':'YO! Max Value is 5'
+            }
+        }
